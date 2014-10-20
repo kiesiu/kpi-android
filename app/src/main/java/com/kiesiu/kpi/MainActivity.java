ceeb95f3a0b8ce1b32f59b7ce03c3b763a9e246b
Łukasz Kieś <kiesiu@pld-linux.org>
2014-10-20 19:34:48 +0200

    Added menu placeholder for MainActivity.

--- app/src/main/java/com/kiesiu/kpi/MainActivity.java
+++ app/src/main/java/com/kiesiu/kpi/MainActivity.java
@@ -17,6 +17,8 @@ package com.kiesiu.kpi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -24,5 +26,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
